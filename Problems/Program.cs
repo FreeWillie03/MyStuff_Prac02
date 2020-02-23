fc5d0886f1fccec77378c36eb4b267534c71dee4
Freewillie03 <staplewm@gmail.com>
2020-02-23 18:50:14 +0200

    Renamed Program to problems, added sln and all .cs and .csproj from problems, problems.test

---
+++ Problems/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Problems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!");
+        }
+
+        public string Prefix(string input){
+            int length = input.Length;
+            int words = 1;
+            string output;
+
+            if(input == null){
+                return null;
+            }
+
+            foreach(char c in input){
+                if(c ==' '){
+                    words++;
+                }
+            }
+
+            if(length > 0){
+                output = string.Format("{1},{2}:{3}",length,words,input);
+            }else{
+                words--;
+                output = string.Format("{1},{2}:{3}",length,words,input);
+            }
+
+            return output;
+        }
+    }
+}
